f473beaaf81b4e97a2a9c1bbc129cb18f1601c17
Sergey <srega-bilozor@rambler.ru>
2017-12-13 09:00:48 +0200

    1ver

---
+++ ConsoleApplication9.cpp
@@ -0,0 +1,40 @@
+// ConsoleApplication9.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include "time.h"
+#include "windows.h"
+#include "iostream"
+
+using namespace std;
+
+int main()
+{
+	int A[10];
+	srand(time(NULL));
+	for (int i = 0; i < 10; i++)
+	{
+		A[i] = rand() % 21;
+		cout <<"A["<< i <<"] = "<< A[i] << endl;
+	}
+	cout << "Elementy kratnie 6:" << endl;
+	for (int i = 0; i < 10; i++)
+	{
+		if (A[i] % 6 == 0)
+		{
+			cout << "A[" << i << "] = " << A[i] << endl;
+		}
+	}
+	int min = A[0];
+	for (int i = 0; i < 10; i++)
+	{
+		if (A[i] < min)
+		{
+			min = A[i];
+		}
+	}
+	cout << "Min element = " << min << endl;
+	system("pause");
+	return 0;
+}
+
